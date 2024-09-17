a95f31e085d25f5bcffefa569dac885a2edb5767
Patrick Coutinho <rodrigo@MacBook-Pro-de-Rodrigo.local>
2024-09-16 23:09:45 -0300

    up schedule

--- src/context/ScaleContext.tsx
+++ src/context/ScaleContext.tsx
@@ -36,20 +36,20 @@ const schedules = {
       { date: "01/09/2024", teclas: musicians["teclas.1"], batera: musicians["batera.2"], bass: musicians["bass.2"], guita: musicians["guita.2"], vocal1: musicians["vocal.3"], vocal2: musicians["vocal.2"] },
       { date: "08/09/2024", teclas: musicians["teclas.1"], batera: musicians["batera.2"], bass: musicians["bass.3"], guita: musicians["guita.3"], vocal1: musicians["vocal.1"], vocal2: musicians["vocal.2"] },
       { date: "15/09/2024", teclas: musicians["teclas.1"], batera: musicians["batera.3"], bass: musicians["bass.1"], guita: musicians["guita.1"], vocal1: musicians["vocal.5"], vocal2: musicians["vocal.8"] },
-      { date: "22/09/2024", teclas: musicians["teclas.1"], batera: musicians["batera.2"], bass: musicians["bass.2"], guita: musicians["guita.2"], vocal1: musicians["vocal.9"], vocal2: musicians[""] },
+      { date: "22/09/2024", teclas: musicians["teclas.1"], batera: musicians["batera.2"], bass: musicians["bass.2"], guita: musicians["guita.4"], vocal1: musicians["vocal.9"], vocal2: musicians[""] },
       { date: "29/09/2024", teclas: musicians["teclas.1"], batera: musicians["batera.2"], bass: musicians["bass.3"], guita: musicians["guita.3"], vocal1: musicians["vocal.3"], vocal2: musicians["vocal.4"] }
     ],
     Outubro: [
       { date: "06/10/2024", teclas: musicians["teclas.1"], batera: musicians["batera.1"], bass: musicians["bass.1"], guita: musicians["guita.1"], vocal1: musicians["vocal.1"], vocal2: musicians["vocal.4"] },
       { date: "13/10/2024", teclas: musicians["teclas.1"], batera: musicians["batera.2"], bass: musicians["bass.2"], guita: musicians["guita.2"], vocal1: musicians["vocal.3"], vocal2: musicians["vocal.2"] },
       { date: "20/10/2024", teclas: musicians["teclas.1"], batera: musicians["batera.3"], bass: musicians["bass.3"], guita: musicians["guita.3"], vocal1: musicians["vocal.1"], vocal2: musicians["vocal.4"] },
-      { date: "27/10/2024", teclas: musicians["teclas.1"], batera: musicians["batera.1"], bass: musicians["bass.1"], guita: musicians["guita.1"], vocal1: musicians["vocal.1"], vocal2: musicians["vocal.2"] }
+      { date: "27/10/2024", teclas: musicians["teclas.1"], batera: musicians["batera.1"], bass: musicians["bass.1"], guita: musicians["guita.4"], vocal1: musicians["vocal.1"], vocal2: musicians["vocal.2"] }
     ],
     Novembro: [
       { date: "03/11/2024", teclas: musicians["teclas.1"], batera: musicians["batera.2"], bass: musicians["bass.2"], guita: musicians["guita.2"], vocal1: musicians["vocal.3"], vocal2: musicians["vocal.4"] },
-      { date: "10/11/2024", teclas: musicians["teclas.1"], batera: musicians["batera.2"], bass: musicians["bass.3"], guita: musicians["guita.3"], vocal1: musicians["vocal.1"], vocal2: musicians["vocal.2"] },
-      { date: "17/11/2024", teclas: musicians["teclas.1"], batera: musicians["batera.3"], bass: musicians["bass.1"], guita: musicians["guita.1"], vocal1: musicians["vocal.3"], vocal2: musicians["vocal.4"] },
-      { date: "24/11/2024", teclas: musicians["teclas.1"], batera: musicians["batera.2"], bass: musicians["bass.2"], guita: musicians["guita.2"], vocal1: musicians["vocal.1"], vocal2: musicians["vocal.2"] }
+      { date: "10/11/2024", teclas: musicians["teclas.1"], batera: musicians["batera.2"], bass: musicians["bass.3"], guita: musicians["guita.1"], vocal1: musicians["vocal.1"], vocal2: musicians["vocal.2"] },
+      { date: "17/11/2024", teclas: musicians["teclas.1"], batera: musicians["batera.3"], bass: musicians["bass.1"], guita: musicians["guita.4"], vocal1: musicians["vocal.3"], vocal2: musicians["vocal.4"] },
+      { date: "24/11/2024", teclas: musicians["teclas.1"], batera: musicians["batera.2"], bass: musicians["bass.3"], guita: musicians["guita.3"], vocal1: musicians["vocal.1"], vocal2: musicians["vocal.2"] }
     ],
     Dezembro: [
       { date: "01/12/2024", teclas: musicians["teclas.1"], batera: musicians["batera.2"], bass: musicians["bass.3"], guita: musicians["guita.3"], vocal1: musicians["vocal.3"], vocal2: musicians["vocal.4"] },
