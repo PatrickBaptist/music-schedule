c815f70dbe22224beb4f453e52583da6aa3d31f3
Patrick Coutinho <rodrigo@MacBook-Pro-de-Rodrigo.local>
2024-09-17 00:19:27 -0300

    atualization dvh

--- src/pages/pageStyle/SchedulePage.ts
+++ src/pages/pageStyle/SchedulePage.ts
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 export const ContainerSchedule = styled.div`
   width: 100%;
-  height: 100vh;
+  height: 100dvh;
   display: flex;
   flex-direction: column;
   align-items: center;
